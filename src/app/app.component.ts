22f50b62f1040c20debe132a81e951b3e876ee6c
mariaortegadelmazo <mariaortegadelmazo97@gmail.com>
2022-10-17 18:58:37 +0200

    error correction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  /* public title: string = 'Contador App';
-  public counterValue: number = 10;
+  public title: string = '01-bases';
+  /*public counterValue: number = 10;
   public base :number = 5;
 
   counter(operation: string){
